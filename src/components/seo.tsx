d42fbd98876e526f7b93466add0d1ddfafba9225
yuebaiXu <xyb1114@gmail.com>
2021-01-01 23:12:04 +0100

    feat: update-seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,14 +1,21 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-const SEO = ({ title = "Albert Yuebai XU" }: { title?: string }) => (
+const SEO = ({
+  title = "Albert Yuebai XU - Javascript Developer",
+}: {
+  title?: string
+}) => (
   <Helmet>
     <meta charSet="utf-8" />
     <title>{title}</title>
-    <meta name="description" content="Albert Yuebai XU website and portfolio" />
+    <meta
+      name="description"
+      content="Albert Yuebai XU - Javascript Developer in Paris"
+    />
     <meta
       name="keywords"
-      content="Yuebai, XU, Albert, frontend, developer, resume, portfolio, 许月白"
+      content="Yuebai, XU, Albert, frontend, developer, resume, portfolio, 许月白, javascript, full stack"
     />
     <meta name="author" content="Albert Yuebai XU" />
     <meta name="viewport" content="width=device-width, initial-scale = 1.0" />
@@ -17,11 +24,11 @@ const SEO = ({ title = "Albert Yuebai XU" }: { title?: string }) => (
     <meta property="og:url" content="http://www.yuebaixu.com" />
     <meta
       property="og:description"
-      content="Albert Yuebai XU website and portfolio"
+      content="Albert Yuebai XU - Javascript Developer in Paris"
     />
     <meta
       property="og:site_name"
-      content="Albert Yuebai XU, front-end developer"
+      content="Albert Yuebai XU - Javascript Developer in Paris"
     />
     <meta name="robots" content="index, follow" />
   </Helmet>
