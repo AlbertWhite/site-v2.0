9302aa99c77e12460cd99aeb38355fc3331344af
yuebaiXu <xyb1114@gmail.com>
2024-11-06 16:32:42 +0100

    fix: responsive design

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,22 +4,12 @@ import styled from "styled-components"
 import Header from "./header"
 import Footer from "./footer"
 
-const StyledContainer = styled.div`
-  align-items: center;
-  max-width: 672px;
-  margin: 0 auto;
-
-  @media (max-width: 768px) {
-    max-width: 80%;
-  }
-`
-
 export default ({ children }: any) => {
   return (
-    <StyledContainer>
+    <>
       <Header />
       {children}
       <Footer />
-    </StyledContainer>
+    </>
   )
 }
